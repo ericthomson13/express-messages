0e5de0e5e5812dcfbfa6839ab28a8ebbdfac3295
et <eric.thomson13@gmail.com>
2020-01-14 14:34:52 -0700

    Changed Routes to Match Config

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bp.json());
 const port = 3000;
 
 // create message route
-app.post('/message', (req, res) => {
+app.post('/messages', (req, res) => {
 	console.log(req.body);
 	Message.createMessage(req.body)
 		.then(() => {
@@ -21,7 +21,7 @@ app.post('/message', (req, res) => {
 })
 
 // readAll route
-app.get('/message', (req, res) => {
+app.get('/messages', (req, res) => {
 	// need to account for async
 	Message.readAllMessages()
 	.then(result => {
@@ -36,7 +36,7 @@ app.get('/message', (req, res) => {
 })
 
 // readOne route
-app.get('/message/:id', (req, res) => {
+app.get('/messages/:id', (req, res) => {
 	// need to account for async
 	Message.readMessage(req.body)
 		.then((result) => {
@@ -47,7 +47,7 @@ app.get('/message/:id', (req, res) => {
 })
 
 // update route
-app.put('/message/:id', (req, res) => {
+app.put('/messages/:id', (req, res) => {
 	Message.updateMessage(req.body)
 		.then(() => res.send(202))
 		.catch(err => {
@@ -57,7 +57,7 @@ app.put('/message/:id', (req, res) => {
 })
 
 // delete route
-app.delete('message/:id', (req, res) => {
+app.delete('messages/:id', (req, res) => {
 	Message.deleteMessage(req.body)
 		.then(() => res.send(204))
 		.catch(err => res.send(err))
